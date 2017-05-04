df85932ab2551ad19e7e773e530fe25bb1f37c4a
asanka565 <asanka565@PR35.hhs.win.fuhsd.org>
2017-05-04 13:45:41 -0700

    Add filesfrom AnimationDemo.zip Made mario fall

---
+++ src/Mario.java
@@ -0,0 +1,43 @@
+
+
+import java.awt.*;
+import javax.swing.*;
+import java.awt.image.*;
+import java.util.*;
+
+public class Mario extends MovingImage {
+
+	public static final int MARIO_WIDTH = 40;
+	public static final int MARIO_HEIGHT = 60;
+
+
+	public double vX, vY;
+	
+	public Mario(int x, int y) {
+		super("mario.png", x, y, MARIO_WIDTH, MARIO_HEIGHT);
+		
+
+		vX = 0;
+		vY = 0;
+		
+	}
+
+	// METHODS
+	public void walk(int dir) {
+		moveByAmount(dir, 0);
+		// WALK!
+	}
+
+	public void jump() {
+		// JUMP!
+	}
+
+	public void act(ArrayList<Shape> obstacles) {
+		
+		vY += 0.3;
+		y+= vY;
+	
+	}
+
+
+}
